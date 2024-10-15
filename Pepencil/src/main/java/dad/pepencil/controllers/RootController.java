3d3b52e73744efdf37534fa0a1b49458b65bc90c
Jorgell01 <yacolua@outlook.com>
2024-10-15 08:57:24 +0100

    Proyecto pepencil actualizado

---
+++ Pepencil/src/main/java/dad/pepencil/controllers/RootController.java
@@ -0,0 +1,26 @@
+package dad.pepencil.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+
+public class RootController {
+
+    @FXML
+    private TabPane editionTabPane;
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    void onNewAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOpenAction(ActionEvent event) {
+
+    }
+
+}
